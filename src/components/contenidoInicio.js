593a0f7db40dd313bebf77e6df5e8fc230ca65ef
MiguelMolina29 <miguelantonio.molinago@my.uvm.edu.mx>
2021-08-22 17:57:26 -0500

    Css-fluid

--- src/components/contenidoInicio.js
+++ src/components/contenidoInicio.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image'
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const TextoInicio = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media(min-width:768px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 2rem;
+    }
+
+    p {
+        line-height: 2;
+        margin-top: 2rem;
+    }
+`;
 
 const ContenidoInicio = () => {
     const informacion = useStaticQuery(graphql`
@@ -25,12 +45,18 @@ const ContenidoInicio = () => {
 
     return (
         <>
-            <h2>{titulo}</h2>
+            <h2
+                css={css`
+                    text-align: center;
+                    font-size: 4rem;
+                    margin-top: 4rem;
+                `}
+            >{titulo}</h2>
 
-            <div>
+            <TextoInicio>
                 <p>{contenido}</p>
                 <Img fluid={imagen.fluid} />
-            </div>
+            </TextoInicio>
 
             
         </>
